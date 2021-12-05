b69e5d54f7cc1ee0f35d9d2c156057bab60da6d2
SAVAGE <89578700+SAVAGE1300@users.noreply.github.com>
2021-12-05 14:42:05 +0500

    lol

---
+++ ind_1.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("container").style.transform = "scale(1.2)"
+    document.getElementById("container").style.transitionDuration = "2s"
+}
+function myFunction1() {
+    document.getElementById("jum4").style.backgroundImage = "linear-gradient(to right, red, black);"
+}
